cbde0d8fd00191d9ff9458d70e98d94c0d5d6b09
BigBigBai <baiyingjian@outlook.com>
2025-07-19 10:25:18 -0400

    Database connect

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express';
+import products from './data/products.js';
 import dotenv from 'dotenv';
 dotenv.config();
-import products from './data/products.js';
-const port = process.env.PORT || 5001;
+import connectDB from './config/db.js';
+
+const port = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 
@@ -19,4 +23,6 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+);
