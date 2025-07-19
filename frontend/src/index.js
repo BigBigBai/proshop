6d3b4fec837b0af2f6c9fb1573c3fcf6655a1579
BigBigBai <baiyingjian@outlook.com>
2025-07-19 05:14:47 -0400

    Added Link and LinkContainer for routing

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -14,8 +14,8 @@ import HomeScreen from './screens/HomeScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<App />}>
-      <Route index={true} path='/' element={<HomeScreen />} />
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
     </Route>
   )
 );
